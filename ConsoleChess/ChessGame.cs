b7309a0bf1ca3360280f98e4abe9ca3296bd495b
hamhub7 <hamhub7@yahoo.com>
2023-03-03 10:13:50 -0500

    Added more endgame conditions, notably draw material and the 50-move rule

--- ConsoleChess/ChessGame.cs
+++ ConsoleChess/ChessGame.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleChess
+﻿using ConsoleChess.Pieces;
+
+namespace ConsoleChess
 {
     public class ChessGame
     {
@@ -29,7 +31,24 @@
 
             ActivePlayer = ActivePlayer == PieceColor.White ? PieceColor.Black : PieceColor.White;
 
-            if (!GetMoves().Any()) return true;
+            // Check endgame conditions
+            if (!GetMoves().Any()) return true; // No legal moves, means checkmate or stalemate
+            if (board.MovesSoFar.Skip(Math.Max(0, board.MovesSoFar.Count - 50)).Where(m => m.MovingPiece is not Pawn && m.CapturedPiece is null).Any()) return true; //None of the last 50 moves have captured a piece or moved a pawn. 50-move rule
+            //implement repetition?
+            if(!board.KingChecked(ActivePlayer)) // Implement material. make sure the king isnt in check before calculating material
+            {
+                if(!board.Pieces.Any(p => p is Pawn || p is Rook || p is Queen)) // Any of these pieces means we have sufficient material
+                {
+                    int whiteKnights = board.Pieces.Where(p => p.Color == PieceColor.White && p is Knight).Count();
+                    int blackKnights = board.Pieces.Where(p => p.Color == PieceColor.Black && p is Knight).Count();
+                    int whiteMinors = board.Pieces.Where(p => p.Color == PieceColor.White && (p is Bishop || p is Knight)).Count();
+                    int blackMinors = board.Pieces.Where(p => p.Color == PieceColor.Black && (p is Bishop || p is Knight)).Count();
+
+                    if (whiteMinors <= 1 && blackMinors <= 1) return true; // King v King, Minor + King v King, or Minor + King v Minor + King
+                    if (whiteMinors == whiteKnights && whiteKnights == 2 && blackMinors == 0) return true; // 2 Knights + King v King
+                    if (blackMinors == blackKnights && blackKnights == 2 && whiteMinors == 0) return true; // King v 2 Knights + King
+                }
+            }
             return false;
         }
 
